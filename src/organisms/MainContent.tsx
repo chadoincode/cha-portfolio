b5a6762bc61dbb1c193f1b5af00314fdc5085f3f
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-27 21:47:09 +0700

    feat: fill maincontent comps

--- src/organisms/MainContent.tsx
+++ src/organisms/MainContent.tsx
@@ -7,8 +7,34 @@ const MainContent = () => {
       <div className="py-5 px-15 sm:py-11 sm:px-20 text-light w-[600px] md:w-[900px]">
         <div className="text-justify sm:text-left grid gap-3">
           <h1 className="text-lg font-bold sm:text-3xl">Hi, I'm Ayesha!!</h1>
+          <p className="sm:text-md">
+            An Informatics Engineering student passionate about Web Development and Video Editing.
+            Crafting simple, functional, and meaningful digital experiences.
+          </p>
+          <p className="sm:text-md">
+            I enjoy building simple web applications and exploring creativity through video editing and graphic design.
+            I have interest in Video Editing, Graphic Design, Front-End Development.
+            Currently, I'm sharpening my skills in React.js, SEO, Adobe Premiere Pro, and Adobe After Effects.
+          </p>
+          <p className="sm:text-md">
+            When I'm not coding or editing, you can find me playing games / reading books / exploring new ideas.
+          </p>
+          <p className="sm:text-md">
+            You can look all of my projects and my creations in <a className="underline" href="/projects">Projects</a>
+          </p>
+          <p className="sm:text-md">
+            Also my designs and sum arts in <a className="underline" href="/gallery">Gallery</a>
+          </p>
+          <p>
+            My other Socials:
+          </p>
+          <div className="flex justify-start gap-2">
+
+          </div>
         </div>
       </div>
     </div>
   )
 }
+
+export default MainContent
