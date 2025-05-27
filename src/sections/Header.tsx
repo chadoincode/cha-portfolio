ea5244420b89e7a4fc53fd9506a5a3029ebf346a
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-27 21:10:11 +0700

    chore: add header component

---
+++ src/sections/Header.tsx
@@ -0,0 +1,14 @@
+import { Github } from "lucide-react";
+import { Instagram } from "lucide-react";
+import { Linkedin } from "lucide-react";
+import { Mail } from "lucide-react";
+import { HeartIcon } from "lucide-react";
+import { PanelsLeftBottomIcon } from "lucide-react";
+
+const Header = () => {
+  return(
+    <div>
+      <a href="/" title="Home" className="flex items-center justify-between sm:px-20 left-0 px-10 py-5">chadoincode</a>
+    </div>
+  )
+}
