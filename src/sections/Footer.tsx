534c97d2493f62fbd52a04aaf806175d0fe99726
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-27 21:31:52 +0700

    feat(moleculs): add footer comps

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return(
+    <div className="text-light text-xs flex justify-center p-10">
+      <p>Created with 💻, 🧋, and 💜 by <a href="https://github.com/chadoincode" className="underline">chadoincode</a></p>
+    </div>
+  )
+}
+
+export default Footer
