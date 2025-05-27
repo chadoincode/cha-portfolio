dce20395e13fe90100663ec2cd6e1cb49d801253
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-27 21:13:48 +0700

    chore: edit sum change in header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -7,8 +7,10 @@ import { PanelsLeftBottomIcon } from "lucide-react";
 
 const Header = () => {
   return(
-    <div>
-      <a href="/" title="Home" className="flex items-center justify-between sm:px-20 left-0 px-10 py-5">chadoincode</a>
+    <div className="flex items-center justify-between sm:px-20 left-0 px-10 py-5">
+      <a href="/" title="Home" className="font-bold sm:text-lg text-md text-light">chadoincode</a>
     </div>
   )
 }
+
+export default Header
