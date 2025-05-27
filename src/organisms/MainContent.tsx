b0c8562f94086fb3f48d9d3901dcc29a6922ccef
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-27 21:38:10 +0700

    feat: add maincontent comps

---
+++ src/organisms/MainContent.tsx
@@ -0,0 +1,14 @@
+import { Instagram } from "lucide-react";
+import { Github } from "lucide-react";
+
+const MainContent = () => {
+  return(
+    <div className="flex place-items-center justify-center">
+      <div className="py-5 px-15 sm:py-11 sm:px-20 text-light w-[600px] md:w-[900px]">
+        <div className="text-justify sm:text-left grid gap-3">
+          <h1 className="text-lg font-bold sm:text-3xl">Hi, I'm Ayesha!!</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
