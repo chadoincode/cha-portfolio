137a229d89d7c8a18ff940c7bc66c8ff75306619
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-28 13:55:07 +0700

    feat: add title comps

--- src/organisms/ProjectsContent.tsx
+++ src/organisms/ProjectsContent.tsx
@@ -4,11 +4,10 @@ const ProjectsContent = () => {
   return(
     <div className="flex place-items-center justify-center">
       <div className="py-5 px-15 sm:py-11 sm:px-20 text-light w-[600px] md:w-[900px]">
-        <div className="">
+        <div className="text-justify sm: ">
 
         </div>
       </div>
-      
     </div>
   )
 }
