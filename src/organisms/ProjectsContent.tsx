83c0d62929c9fbac5faf072168b4ff825251c378
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-28 11:30:56 +0700

    feat(projects): add projectscontent comps

--- src/organisms/ProjectsContent.tsx
+++ src/organisms/ProjectsContent.tsx
@@ -1,6 +1,13 @@
+import FadeIn from "src/animations/FadeIn"
+
 const ProjectsContent = () => {
   return(
-    <div>
+    <div className="flex place-items-center justify-center">
+      <div className="py-5 px-15 sm:py-11 sm:px-20 text-light w-[600px] md:w-[900px]">
+        <div className="">
+
+        </div>
+      </div>
       
     </div>
   )
