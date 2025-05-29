66e6a2cc49e82ca68e037c5b6d24b2aaebf8c8aa
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-05-29 22:06:00 +0700

    feat: add web projects data

---
+++ src/datas/WebProjects.ts
@@ -0,0 +1,17 @@
+export const WebProjects = [
+  {
+    name: "Soulful Dhikr",
+    description: "A simple web that allows user to read and navigate dhikr and daily dua.",
+    link: "https://www.github.com/chadoincode/soulful-dhikr"
+  },
+  {
+    name: "Web Portfolio",
+    description: "This is my personal portfolio website. Showcasing my skills, projects, and experiences.",
+    link: ""
+  },
+  {
+    name: "Brutal Music API",
+    description: "An API that contains a collection of Viva Fantasy, BPK SMP, and Brutal Legends soundtracks.",
+    link: "https://github.com/chadoincode/brutal-music-api"
+  }
+]
