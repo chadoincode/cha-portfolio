7b8e400b64b72511daaf0a82ee8e6a6d393bd1a2
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-13 15:19:51 +0700

    chore: add data for skill

---
+++ src/datas/Skills.ts
@@ -0,0 +1,42 @@
+export const skills = [
+  {
+    name: "ReactJS",
+    icon: "react",
+    category: "Front-End, Web Development"
+  },
+  {
+    name: "Astro",
+    icon: "",
+    category: "Front-End, Web Development"
+  },
+  {
+    name: "Github",
+    icon: "", 
+    category: "Web Development",
+  },
+  {
+    name: "JavaScript",
+    icon: "",
+    category: "Front-End, Web Development"
+  },
+  {
+    name: "TypeScript",
+    icon: "",
+    category: "Front-End, Web Development"
+  },
+  {
+    name: "Adobe Premiere Pro",
+    icon: "",
+    category: "Video Editing"
+  },
+  {
+    name: "Adobe After Effects",
+    icon: "",
+    category: "Video Editing, Motion Graphic"
+  },
+  {
+    name: "Figma",
+    icon: "",
+    category: "Graphic Design"
+  }
+]
