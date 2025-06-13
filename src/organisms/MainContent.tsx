9427573147eb775ff6efdf018351003ccc495f25
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-13 10:44:35 +0700

    chore: add scroll animation

--- src/organisms/MainContent.tsx
+++ src/organisms/MainContent.tsx
@@ -1,4 +1,5 @@
 import FadeIn from "../animations/FadeIn"
+import ScrollAnimation from "src/animations/ScrollAnimation"
 import Button from "../atoms/Button"
 import { Instagram } from "lucide-react"
 import { Github } from "lucide-react"
@@ -8,42 +9,43 @@ const MainContent = () => {
     <div className="flex place-items-center justify-center">
       <div className="py-5 px-15 sm:py-11 sm:px-20 text-light w-[600px] md:w-[900px]">
         <div className="text-justify sm:text-left grid gap-3">
-          <FadeIn delay={0.2}>
+          <ScrollAnimation>
             <h1 className="text-lg font-bold sm:text-3xl">Hi, I'm Ayesha</h1>
-          </FadeIn>
-          <FadeIn delay={0.3}>
+          </ScrollAnimation>
+          <ScrollAnimation>
             <p className="sm:text-md">
               An Informatics Engineering student passionate about Web Development and Video Editing.
               Crafting simple, functional, and meaningful digital experiences.
             </p>
-          </FadeIn>
-          <FadeIn delay={0.4}>
+          </ScrollAnimation>
+          <ScrollAnimation>
             <p className="sm:text-md">
               I enjoy building simple web applications and exploring creativity through video editing and graphic design.
               I have interest in Video Editing, Graphic Design, Front-End Development.
               Currently, I'm sharpening my skills in React.js, SEO, Adobe Premiere Pro, and Adobe After Effects.
             </p>
-          </FadeIn>
-          <FadeIn delay={0.5}>
+          </ScrollAnimation>
+          <ScrollAnimation>
             <p className="sm:text-md">
               When I'm not coding or editing, you can find me playing games / reading books / exploring new ideas.
             </p>
-          </FadeIn>
-          <FadeIn delay={0.6}>
+          </ScrollAnimation>
+          
+          <ScrollAnimation>
             <p className="sm:text-md">
               You can look all of my projects and my creations in <a className="underline" href="/projects">Projects</a>
             </p>
             <p className="sm:text-md">
               Also my designs and some arts in <a className="underline" href="/gallery">Gallery</a>
             </p>
-          </FadeIn>
-          <FadeIn delay={0.7}>
+          </ScrollAnimation>
+          <ScrollAnimation>
             <p>
               My other Socials:
             </p>
-          </FadeIn>
+          </ScrollAnimation>
           <div className="flex justify-start gap-2">
-            <FadeIn delay={0.8}>
+            <ScrollAnimation>
               <div className="p-3 bg-hover-card w-fit rounded-lg cursor-pointer text-sm">
                 <Button
                   text="Instagram"
@@ -53,8 +55,8 @@ const MainContent = () => {
                     <Instagram size={20} />
                   </Button>
               </div>
-            </FadeIn>
-            <FadeIn delay={0.9}>
+            </ScrollAnimation>
+            <ScrollAnimation>
               <div className="p-3 bg-hover-card w-fit rounded-lg cursor-pointer text-sm">
                 <Button
                   text="Github"
@@ -64,7 +66,7 @@ const MainContent = () => {
                     <Github size={20} />
                   </Button>
               </div>
-            </FadeIn>
+            </ScrollAnimation>
           </div>
         </div>
       </div>
--- src/organisms/ProjectsContent.tsx
+++ src/organisms/ProjectsContent.tsx
@@ -1,17 +1,18 @@
 import ProjectsTitle from "src/atoms/ProjectsTitle.tsx"
 import ProjectsCard from "src/atoms/ProjectsCard.tsx"
 import FadeIn from "src/animations/FadeIn"
+import ScrollAnimation from "src/animations/ScrollAnimation"
 import { WebProjects } from "src/datas/WebProjects"
 
 const ProjectsContent = () => {
   return(
     <div className="flex-min-h-screen px-25 py-10">
-      <FadeIn delay={0.2}>
+      <ScrollAnimation>
         <div className="flex-col place-items-center text-light">
           <h1 className="text-2xl font-bold">Projects</h1>
           <p>Contains all of my works!</p>
         </div>
-      </FadeIn>
+      </ScrollAnimation>
       <FadeIn delay={0.3}>
         <div className="py-5 text-light">
           <ProjectsTitle category="Simple Websites" />
@@ -29,6 +30,14 @@ const ProjectsContent = () => {
             </FadeIn>
           ))
         }
+      </div>
+      <ScrollAnimation>
+        <div>
+          <ProjectsTitle category="Video Editing" />
+        </div>
+      </ScrollAnimation>
+      <div>
+        {}
       </div>
     </div>
   )
