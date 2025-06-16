30973af1c8df685c206d2e5fa002049f02697783
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-16 10:42:40 +0700

    feat(gallery): add designs data

---
+++ src/datas/Designs.ts
@@ -0,0 +1,15 @@
+import MatchaLatte from "../assets/designs/matcha-latte.png"
+import SoftServe from "../assets/designs/soft-serve.png"
+import Flexy from "../assets/designs/flexy-poster.jpg"
+
+export const designs = [
+  {
+    image: Flexy,
+  },
+  {
+    image: MatchaLatte
+  },
+  {
+    image: SoftServe
+  }
+]
