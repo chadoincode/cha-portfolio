956136640e215e96d54b59bc2c908e6b6476acbd
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-16 10:48:20 +0700

    feat(gallery): create design card

--- src/atoms/DesignCard.tsx
+++ src/atoms/DesignCard.tsx
@@ -2,3 +2,16 @@ type DesignCardProps = {
   title: string
   image: string
 }
+
+const DesignCard = ({ title, image}: DesignCardProps) => {
+  return(
+    <div>
+      <div>
+        <img src={image} alt={title} className="w-50" />
+      </div>
+      <div>
+        <p>{title}</p>
+      </div>
+    </div>
+  )
+}
