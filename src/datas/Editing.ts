111eb25895a8cf02bebc16dbca04e76595098cfe
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-17 17:41:47 +0700

    fix(projects): fix bug in projects image path

--- src/datas/Editing.ts
+++ src/datas/Editing.ts
@@ -1,9 +1,7 @@
-import KNN from "../assets/thumbnails/knn.png"
-
 export const editing = [
   {
-    "title": "Algoritma K-Nearest Neighbor",
-    "link": "https://youtu.be/FmEP1utVOgo",
-    "thumbnail": KNN
+    title: "Algoritma K-Nearest Neighbor",
+    link: "https://youtu.be/FmEP1utVOgo",
+    thumbnail: "/thumbnails/knn.png"
   }
 ]
