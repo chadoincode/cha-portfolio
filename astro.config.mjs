287894d1b12365a3123bc6a79f0ec316ff8a8b9c
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-17 18:01:41 +0700

    fix: fix error in path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react'
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   vite:{
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
     plugins: [tailwindcss()]
 },
   integrations: [react()]
