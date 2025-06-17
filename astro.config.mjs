06243e9328c9b63dd6dc8d25dc1554f910523622
Ayesha Dear Raisa <ayeshacha177@gmail.com>
2025-06-17 20:10:29 +0700

    fix: fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path';
-console.log('[VITE CONFIG] alias @ =', path.resolve('./src'));
+// console.log('[VITE CONFIG] alias @ =', path.resolve('./src'));
 
 // https://astro.build/config
 export default defineConfig({
   vite:{
     resolve: {
       alias: {
-        '@': path.resolve('./src'),
+        '@': path.resolve('./src')
       },
     },
     plugins: [tailwindcss()]
