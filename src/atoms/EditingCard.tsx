f6c72c96da5db6ab1770f3119bed0cb8b6ed0ded
shaa/cha(?!) <ayeshacha177@gmail.com>
2025-06-21 20:17:01 +0700

    chore: add lazy loading

--- src/atoms/EditingCard.tsx
+++ src/atoms/EditingCard.tsx
@@ -8,7 +8,7 @@ const EditingCard = ({ title, link, thumbnail }: EditingCardProps) => {
   return(
     <div className="group relative overflow-hidden cursor-pointer border-2 border-light hover:bg-hover-card rounded-lg transition-all duration-300">
       <div className="duration-300 inset-0 transition-opacity group-hover:opacity-0">
-        <img src={thumbnail} alt={title} className="w-70" />
+        <img src={thumbnail} alt={title} className="w-70" loading="lazy" />
       </div>
       <a href={link} target="_blank">
         <div className="absolute inset-0 flex items-center justify-center opacity-0 duration-300 translate-y-10 hover:opacity-100 group-hover:translate-y-0">
