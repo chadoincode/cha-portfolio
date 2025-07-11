1e93b07beb201b7629a7bf542de99b9a8865b209
shaa/cha(?!) <ayeshacha177@gmail.com>
2025-07-11 19:02:01 +0700

    chore(edit): add thumbnail image

--- src/datas/Editing.ts
+++ src/datas/Editing.ts
@@ -1,7 +1,14 @@
+import { title } from "process";
+
 export const editing = [
   {
     title: "Algoritma K-Nearest Neighbor",
     link: "https://youtu.be/FmEP1utVOgo",
     thumbnail: "/thumbnails/knn.png"
+  },
+  {
+    title: "Fractured Short Movie",
+    link: "https://youtu.be/-lGEOJz5-QI?si=9SkFdGg7PHtyeYZO",
+    thumbnail
   }
 ]
