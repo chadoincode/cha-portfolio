bdc0841ec6a0f0e980fdb595278d4405ff171d82
shaa/cha(?!) <ayeshacha177@gmail.com>
2025-07-21 13:59:27 +0700

    chore: add video edit skill

--- src/datas/Editing.ts
+++ src/datas/Editing.ts
@@ -8,5 +8,10 @@ export const editing = [
     title: "Fractured Short Movie",
     link: "https://youtu.be/-lGEOJz5-QI?si=9SkFdGg7PHtyeYZO",
     thumbnail: "/thumbnails/tumnil.png"
+  },
+  {
+    title: "e-Recruitment Promotion Video",
+    link: "https://youtu.be/3S5q5-PnNb0",
+    thumbnail: "/thumbnails/erecruitment.jpg"
   }
 ]
